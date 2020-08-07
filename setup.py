ed38df26b93c74f525f28f7724b42174f8b5de77
Gerry Manoim <gmanoim@quantopian.com>
2020-08-06 22:42:24 -0400

    added benchmark as well

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
     install_requires=install_requires,
     extras_require={
         "test": ["pytest"],
+        "benchmark": ["pytest-benchmark"],
     },
     ext_modules=[
         extension(
